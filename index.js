5d3890b1f61c9fe1b81b1c2720dd833291bd221a
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-02 09:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can't hear you!"
   }
-} else if (string.toUpperCase()) {
+} 
+else if (string.toUpperCase()) {
   return "YES INDEED!"
 }
 
